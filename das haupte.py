2bbc50065289a26ea96d0f215c417ed83b61f04c
Sebbiarmy <hiiamsehrcool@gmail.com>
2026-04-28 13:30:22 +0100

    erster versuch

--- das haupte.py
+++ das haupte.py
@@ -1,5 +1,7 @@
 #fidshg,ikrewgjhitg,hfddfjfkjkjfkdfkjbgkjeidhjdsfkjl,gnjdeijlbgfvjfjesadkjfbhdffkjlfkhdfsjilbkjkjfkjnmjbnmjnhjmikjjucvxkjkucxkjkudhjdfkjawjpogh.bsojugswjuorghsw:dsihyutfgrhfgkfrugfhsw'/riugslrgjrfierislgjfdjbvvbnrhgrhgrughruguerguawp/dlf,b,v,gvndidkiw[g;
+import os
 
-
-import pillow
-
+from PIL import Image;
+bildda    = os.path.join(os.path.dirname(__file__) , "718523.png") 
+bild = Image.open(bildda)
+print(bild.format, bild.size, bild.mode)
