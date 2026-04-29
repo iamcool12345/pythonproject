1631bca21573d6b6f7acf1f7cf2e9798ad336026
Sebbiarmy <hiiamsehrcool@gmail.com>
2026-04-29 10:05:39 +0100

    da

--- das haupte.py
+++ das haupte.py
@@ -1,13 +1,12 @@
 #fidshg,ikrewgjhitg,hfddfjfkjkjfkdfkjbgkjeidhjdsfkjl,gnjdeijlbgfvjfjesadkjfbhdffkjlfkhdfsjilbkjkjfkjnmjbnmjnhjmikjjucvxkjkucxkjkudhjdfkjawjpogh.bsojugswjuorghsw:dsihyutfgrhfgkfrugfhsw'/riugslrgjrfierislgjfdjbvvbnrhgrhgrughruguerguawp/dlf,b,v,gvndidkiw[g;
 import os
 import tkinter as tk
-from tkinter import PhotoImage as bild
 from PIL import Image, ImageTk;
 bildda    = os.path.join(os.path.dirname(__file__) , "718523.png") 
 bildedit = Image.open(bildda)
 print(bildedit.format, bildedit.size, bildedit.mode)
 
-
+#"C:\Users\Sebbiarmy\Pictures\0331.mp4 vlcsnap-2026-01-25-20h42m01s288.png"
 class bildui:
     def __init__(self,ui):
         self.x = 1
@@ -16,24 +15,23 @@ class bildui:
         self.x = self.x +1
         self.okbaddne = tk.Button(ui,text="rain(er) DA!",command=self.open)
         self.okbaddne.grid(row=self.x, column=0, columnspan=2) 
-     #   self.testimg = tk.Label(image=bildedit)
-      #  self.test = ImageTk.PhotoImage(bildedit)
-        
+        #self.testimg = tk.Label(image=bildedit)
+        #self.test = ImageTk.PhotoImage(bildedit)
         self.x = self.x +1
-        self.pic = tk.Label(ui)  # attach to ui!
-        self.pic.grid(row=self.x, column=1)
-
+        self.pic = tk.Label(ui) 
+        self.pic.grid(row=self.x, column=0)
         self.x = self.x +1
     def open(self):
         print("sddfkpogf.njgf")
         path = self.name.get()
-        self.setpicture = Image.open(path) 
+        self.setpicture = Image.open(path).rotate(180)
         self.testa=ImageTk.PhotoImage(self.setpicture)
         self.pic.config(image=self.testa)
-        #self.pic.grid(row=self.x,column=1)
-        #self.pic.configure(image=setpicture)
-
+        self.pic.grid(column=0)
 ui = tk.Tk()
-ui.configure(background="#ff3ac6")
+ui.title("irgendwasmitlangenamen")
+ui.configure(background="#000000")
+ui.geometry('1920x1000')
+ui.iconbitmap("icon.ico")
 program = bildui(ui)
 ui.mainloop()
